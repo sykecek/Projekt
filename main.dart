8dadc641323d61e8b6d43adc424fadda6ad2d321
Šimon Sýkora <simon.sykora8@seznam.cz>
2026-01-20 13:54:41 +0100

    Refactor imports and enhance code comments
    Updated import statements and added comments for clarity.

--- main.dart
+++ main.dart
@@ -1,74 +1,84 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'bluetooth_ovladac.dart';
-import 'servo_control.dart';
-import 'sequence_screen.dart';
+import 'package:flutter/material.dart'; ///import pro Flutter framework
+import 'package:get/get.dart'; ///import pro GetX balíček pro správu stavu a navigaci
+import 'bluetooth_ovladac,servo_control/bluetooth_ovladac.dart'; ///import pro BluetoothController třídu
+import 'bluetooth_ovladac,servo_control/servo_control.dart'; ///import pro ServoControlScreen třídu
+import 'sequence_screen.dart'; ///import pro SequenceScreen třídu
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  Get.put(BluetoothController()); // Dependency injection
-  runApp(const MyApp());
+void main() { ///hlavní vstupní bod aplikace, void main() = hlavní funkce bez návratové hodnoty
+  WidgetsFlutterBinding.ensureInitialized(); ///inicializace Flutter widgetů před spuštěním aplikace
+  Get.put(BluetoothController()); ///dependency injection pro BluetoothController, dependency injection = vkládání závislostí
+  runApp(const MyApp()); ///spuštění aplikace s MyApp jako kořenovým widgetem
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Bluetooth Servo Control",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        useMaterial3: true,
+//<-- Hlavní aplikace-->//<-------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+class MyApp extends StatelessWidget { ///StatelessWidget = widget bez vnitřního stavu, StateFulWidget = widget s vnitřním stavem, vnitřní stav znamená, že widget může měnit svůj vzhled na základě interakcí uživatele nebo jiných faktorů, vnější stav znamená, že widget je statický a nemění svůj vzhled po vytvoření
+  const MyApp({super.key}); ///konstruktor třídy MyApp s volitelným klíčem, proč se vytváří MyApp když už je vytvořené v main? Protože main je pouze vstupní bod aplikace a MyApp definuje samotnou strukturu a vzhled aplikace, super.key = předání klíče rodičovské třídě to znamená , že klíč je předán do nadřazené třídy StatelessWidget kvůli správnému fungování widgetu v rámci stromu widgetů
+
+  @override ///přepsání metody build z nadřazené třídy StatelessWidget, přepisujeme protože chceme definovat vlastní chování této metody
+  Widget build(BuildContext context) { ///metoda build, která vytváří a vrací widgety pro zobrazení na obrazovce
+    return GetMaterialApp( ///GetMaterialApp je rozšíření MaterialApp z GetX balíčku, které poskytuje funkce pro správu stavu a navigaci
+      debugShowCheckedModeBanner: false, ///vypnutí debug banneru v pravém horním rohu aplikace
+      title: "Bluetooth Servo Control", ///název aplikace
+      theme: ThemeData( ///téma aplikace
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo), ///barevná schéma založené na semínkové barvě indigo
+        useMaterial3: true, ///použití Material Design 3 stylu
       ),
-      getPages: [
-        GetPage(name: '/', page: () => const HomeScreen()),
-        GetPage(name: '/servo-control', page: () => const ServoControlScreen()),
-        GetPage(name: '/sequence', page: () => const SequenceScreen()),
+      getPages: [ ///seznam stránek pro navigaci v aplikaci
+        GetPage(name: '/', page: () => const HomeScreen()), ///domovská stránka - stránka pro skenování Bluetooth zařízení
+        GetPage(name: '/servo-control', page: () => const ServoControlScreen()), ///stránka pro ovládání serva
+        GetPage(name: '/sequence', page: () => const SequenceScreen()), ///stránka pro sekvenční ovládání serva
       ],
-      initialRoute: '/',
+      initialRoute: '/', ///počáteční trasa aplikace je domovská stránka
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-  final String title = "Bluetooth HC-05 Scanner";
 
-  @override
-  Widget build(BuildContext context) {
-    final BluetoothController btController = Get.find<BluetoothController>();
+//<-- Domovská obrazovka UI Bluetooth-->//<-------------------------------------------------------------------------------------------------------------------------------------------------
 
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            ElevatedButton.icon(
-              icon: const Icon(Icons.bluetooth_searching),
-              label: Obx(() => Text(btController.isScanning.value
-                  ? "Hledání zařízení..."
-                  : "Vyhledat spárovaná zařízení")),
-              onPressed: btController.isScanning.value
-                  ? null
-                  : () => btController.scanDevices(),
+
+/// Domovská obrazovka pro skenování Bluetooth zařízení
+class HomeScreen extends StatelessWidget {///StatelessWidget = widget bez vnitřního stavu
+  const HomeScreen({super.key}); ///konstruktor třídy HomeScreen s volitelným klíčem super.key = předání klíče rodičovské třídě potřebujeme protože StatelessWidget může mít klíč pro správné fungování widgetu v rámci stromu widgetů
+final String title = "Bluetooth HC-05 Scanner"; ///název obrazovky
+
+  @override ///přepsání metody build z nadřazené třídy StatelessWidget
+  Widget build(BuildContext context) { ///metoda build, která vytváří a vrací widgety pro zobrazení na obrazovce
+    final BluetoothController btController = Get.find<BluetoothController>();///získání instance BluetoothController pomocí GetX dependency injection, instance = konkrétní objekt vytvořený z třídy, final bluetoothController = Get.find<BluetoothController>(); = hledá existující instanci BluetoothController, pokud neexistuje, vytvoří novou instanci a vrátí ji, Kde ji hledá? Hledá ji v kontejneru GetX, který spravuje závislosti aplikace
+
+    ///vrácení Scaffold widgetu, který poskytuje základní strukturu obrazovky
+    return Scaffold( ///Scaffold = základní struktura obrazovky v Material Design aplikaci, zá
+      appBar: AppBar(title: Text(title)),///AppBar = horní lišta aplikace, která obvykle obsahuje název obrazovky a navigační prvky
+      body: Padding( ///Padding = widget pro přidání vnitřního odsazení kolem svého dítěte
+        padding: const EdgeInsets.all(16),///vnitřní odsazení 16 pixelů na všech stranách
+        child: Column(///Column = widget pro uspořádání dětí ve vertikálním sloupci
+          children: [ ///seznam dětí widgetu Column
+            ElevatedButton.icon( ///ElevatedButton.icon = tlačítko s ikonou a textem
+              icon: const Icon(Icons.bluetooth_searching), ///ikona tlačítka
+              label: Obx(() => Text(btController.isScanning.value /// Obx = widget pro reaktivní aktualizaci UI na základě změn v datech
+                  ? "Hledání zařízení..." ///text tlačítka při skenování
+                  : "Vyhledat spárovaná zařízení")), ///text tlačítka při nečinnosti
+              onPressed: btController.isScanning.value ///podmíněné přiřazení funkce pro stisknutí tlačítka
+                  ? null ///pokud probíhá skenování, tlačítko je deaktivováno
+                  : () => btController.scanDevices(), ///pokud neprobíhá skenování, spustí se metoda scanDevices
             ),
-            const SizedBox(height: 24),
-            Expanded(
-              child: Obx(
-                () => ListView.builder(
-                  itemCount: btController.devicesList.length,
-                  itemBuilder: (context, index) {
-                    final device = btController.devicesList[index];
-                    return ListTile(
-                      leading: const Icon(Icons.devices),
-                      title: Text(device.name ?? "Neznámé zařízení"),
-                      subtitle: Text(device.address),
-                      trailing: ElevatedButton(
-                        onPressed: () => btController.connectToDevice(device),
-                        child: const Text("Připojit"),
+            const SizedBox(height: 24), ///mezera mezi tlačítkem a seznamem zařízení
+            Expanded( ///Expanded = widget, který rozšiřuje své dítě tak, aby vyplnilo dostupný prostor
+              child: Obx( /// Obx = widget pro reaktivní aktualizaci UI na základě změn v datech
+                () => ListView.builder( ///ListView.builder = widget pro vytváření posuvného seznamu s dynamickým obsahem
+                  itemCount: btController.devicesList.length, ///počet položek v seznamu je délka seznamu zařízení v BluetoothController
+                  itemBuilder: (context, index) { ///funkce pro vytváření jednotlivých položek seznamu
+                    final device = btController.devicesList[index]; ///získání zařízení na aktuální pozici v seznamu
+                    return ListTile(///ListTile = widget pro zobrazení jedné položky v seznamu s ikonou, názvem, podnázvem a akcemi
+                      leading: const Icon(Icons.devices),///ikona zařízení
+                      title: Text(device.name ?? "Neznámé zařízení"),///název zařízení nebo "Neznámé zařízení", pokud název není dostupný
+                      subtitle: Text(device.address),///adresa zařízení
+                      trailing: ElevatedButton(///ElevatedButton = tlačítko s pozadím, trailing = widget zobrazený na konci ListTile
+                        onPressed: () => btController.connectToDevice(device),///funkce pro připojení k zařízení při stisknutí tlačítka
+                        child: const Text("Připojit"),///text tlačítka
                       ),
                     );
                   },
